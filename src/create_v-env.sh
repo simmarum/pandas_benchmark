f73a06fe27ed00b213f81e6cd436914e97c8de5c
Simmarum <mateusz0831@gmail.com>
2020-01-12 10:45:47 +0100

    Add v-env

---
+++ src/create_v-env.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Simple bash script to recreate env for this repository
+# Activate env by typing
+#
+# source ./v-env/bin/activate
+#
+
+set -e
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+rm -rf $SCRIPTPATH/v-env
+
+python3 -m venv $SCRIPTPATH/v-env
+. $SCRIPTPATH/v-env/bin/activate
+
+pip3 install --compile \
+pandas==0.25.3 \
+more-itertools==8.0.2 \
+pyarrow==0.15.1 \
+psutil==5.6.7 \
+jupyter==1.0.0 \
+matplotlib==3.1.2
