79913b87a8a1aed5f16b24347f8db4c031350996
Simmarum <mateusz0831@gmail.com>
2020-01-13 21:59:49 +0100

    Add more calculation in pandas

--- src/benchmark.py
+++ src/benchmark.py
@@ -8,9 +8,10 @@ import create_files as cf
 
 def load_data(l_path):
     _, l_format = os.path.splitext(l_path)
-    if l_format in ['csv', 'csv.gz']:
+    print(l_format)
+    if l_format in ['.csv', '.gz']:
         return pd.read_csv(l_path, header=0)
-    elif l_format in ['snappy.parquet']:
+    elif l_format in ['.parquet']:
         return pd.read_parquet(l_path)
 
 
@@ -31,8 +32,29 @@ def check_files():
 
 def do_benchmark():
     for f in list_files():
-        df = load_data(f)
         print(f)
+        df = load_data(f)
+        distinct_column(df, "str")
+        self_join(df)
+
+
+def distinct_column(df, col_pattern):
+    cols = [col for col in df.columns if col_pattern in col]
+    print("!", cols)
+    x = pd.unique(df[cols].values.ravel())
+
+
+def self_join(df):
+    join_cols = random.choices(df.columns, k=2)
+    cols_left = random.choices(df.columns, k=5)
+    cols_left = list(set(cols_left+join_cols))
+    cols_right = random.choices(df.columns, k=5)
+    cols_right = list(set(cols_right+join_cols))
+    print(cols_left)
+    print(cols_right)
+    print(join_cols)
+
+    x = df.merge(df, how='inner', on=join_cols)
 
 
 def main():
