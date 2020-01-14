0d026c182f77af457f237e3e1500c79ec924d9c5
Simmarum <mateusz0831@gmail.com>
2020-01-14 15:43:26 +0100

    Update database module

--- src/database/db.py
+++ src/database/db.py
@@ -5,7 +5,7 @@ import cred
 # p = 'password'
 
 
-def save_res_to_db(time, cpu, mem):
+def save_res_to_db(time, cpu, mem, avg_pt):
     try:
         con = mysql.connector.connect(user='Ury3EhU5sM', password=cred.p,
                                       host='remotemysql.com',
@@ -13,8 +13,8 @@ def save_res_to_db(time, cpu, mem):
                                       database='Ury3EhU5sM')
         c = con.cursor()
         c.execute(
-            "INSERT INTO res (time,cpu,mem) VALUES (%s,%s,%s);",
-            (time, cpu, mem)
+            "INSERT INTO res (time,cpu,mem,avg_pt) VALUES (%s,%s,%s,%s);",
+            (time, cpu, mem, avg_pt)
         )
         con.commit()
         print(c.rowcount, "record inserted successfully into results table")
@@ -28,7 +28,7 @@ def save_res_to_db(time, cpu, mem):
             print("MySQL connection is closed")
 
 
-def get_statistic(time, cpu, mem):
+def get_statistic(time, cpu, mem, avg_pt):
     try:
         con = mysql.connector.connect(user='Ury3EhU5sM', password=cred.p,
                                       host='remotemysql.com',
@@ -52,22 +52,74 @@ def get_statistic(time, cpu, mem):
             (
                 SELECT count(*) from res
                 WHERE mem > %s
-            ) as no_mem
+            ) as no_mem,
+            (
+                SELECT count(*) from res
+                WHERE avg_pt > %s
+            ) as no_avg_pt,
+            (
+                SELECT count(*) from res
+                WHERE ts >= DATE_SUB(current_timestamp(), INTERVAL 60 MINUTE)
+            ) as no_all_last,
+            (
+                SELECT count(*) from res
+                WHERE time > %s
+                AND ts >= DATE_SUB(current_timestamp(), INTERVAL 60 MINUTE)
+            ) as no_time_last,
+            (
+                SELECT count(*) from res
+                WHERE cpu > %s
+                AND ts >= DATE_SUB(current_timestamp(), INTERVAL 60 MINUTE)
+            ) as no_cpu_last,
+            (
+                SELECT count(*) from res
+                WHERE mem > %s
+                AND ts >= DATE_SUB(current_timestamp(), INTERVAL 60 MINUTE)
+            ) as no_mem_last,
+            (
+                SELECT count(*) from res
+                WHERE avg_pt > %s
+                AND ts >= DATE_SUB(current_timestamp(), INTERVAL 60 MINUTE)
+            ) as no_avg_pt_last
         FROM DUAL;
         """,
-            (time, cpu, mem)
+            (time, cpu, mem, avg_pt, time, cpu, mem, avg_pt)
         )
         records = c.fetchall()
         no_all = records[0][0]
         no_time = records[0][1]
         no_cpu = records[0][2]
         no_mem = records[0][3]
+        no_avg_pt = records[0][4]
+        no_all_last = records[0][5]
+        no_time_last = records[0][6]
+        no_cpu_last = records[0][7]
+        no_mem_last = records[0][8]
+        no_avg_pt_last = records[0][9]
 
+        perc_time = int(((no_time/no_all))*100)
+        perc_cpu = int(((no_cpu/no_all))*100)
+        perc_mem = int(((no_mem/no_all))*100)
+        perc_avg_pt = int((1-(no_avg_pt/no_all))*100)
+
+        perc_time_last = int(((no_time_last/no_all_last))*100)
+        perc_cpu_last = int(((no_cpu_last/no_all_last))*100)
+        perc_mem_last = int(((no_mem_last/no_all_last))*100)
+        perc_avg_pt_last = int((1-(no_avg_pt_last/no_all_last))*100)
+        # order of field:
+        # ####
+        # no_all, no_all_last,
+        # time, perc_time, perc_time_last,
+        # cpu, perc_cpu, perc_cpu_last,
+        # mem, perc_mem, perc_mem_last,
+        # avg_pt, perc_avg_pt, perc_avg_pt_last,
+        # ####
         print(
-            no_all,
-            no_time/no_all,
-            no_cpu/no_all,
-            no_mem/no_all,
+            no_all, no_all_last,
+            time, perc_time, perc_time_last,
+            cpu, perc_cpu, perc_cpu_last,
+            mem, perc_mem, perc_mem_last,
+            avg_pt, perc_avg_pt, perc_avg_pt_last
         )
 
     except Error as e:
@@ -79,5 +131,7 @@ def get_statistic(time, cpu, mem):
             print("MySQL connection is closed")
 
 
-save_res_to_db(1002, 23.3, 23132)
-get_statistic(1002, 23.3, 23132)
+# save_res_to_db(78068, 25, 557441024, 10000)
+# save_res_to_db(81000, 26, 570000000, 9100)
+# get_statistic(78068, 25, 557441024, 10000)
+# get_statistic(80000, 25, 560000000, 9400)
